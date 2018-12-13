6bb005e66b1c1b9b3ac97214c44b62094db296cb
朱培杰 <3183764662@qq.com>
2018-12-13 11:27:07 +0800

    剩余服务初始化并注册到Eureka中心

--- remainingclient/src/main/java/com/imooc/RemainingclientApplication.java
+++ remainingclient/src/main/java/com/imooc/RemainingclientApplication.java
@@ -3,17 +3,16 @@ package com.imooc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
- * 订单服务
+ * 剩余服务
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-public class OrderclientApplication {
+public class RemainingclientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrderclientApplication.class, args);
+		SpringApplication.run(RemainingclientApplication.class, args);
 	}
 
 }
